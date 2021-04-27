5074c493238e185a7b2257a9b56d16eba426797a
towertop <towertop.cn@gmail.com>
2021-04-27 22:56:05 +0800

    Itself: add a node version check function

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
 // tslint:disable no-console
+import path from 'path';
+import semver from 'semver';
+
+async function checkNodeEngine () {
+  const packageJson = require(path.resolve(process.cwd(), 'package.json'));
+  const nodeRange = packageJson?.engines?.node;
+  if (nodeRange) {
+    const nodeVersion = process.version.substring(1);
+    return [semver.satisfies(nodeVersion, nodeRange), nodeVersion, nodeRange];
+  }
+  return [true, '', ''];
+}
 
 export async function main () {
+  const [isSatisfied, version, range] = await checkNodeEngine();
+  if (!isSatisfied) {
+    console.warn(`current node version ${version} do not satisfy ${range}`);
+  }
   console.log('Hello World');
 }
 
